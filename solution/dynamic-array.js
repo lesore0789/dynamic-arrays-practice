5f2cb59f80c8b28873e8a53cda1ecdbcd9a829c3
Leslie <lesore0789@gmail.com>
2023-02-18 00:12:15 -0800

    Completed practice

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,19 +1,30 @@
 class DynamicArray {
 
-  constructor(defaultSize=4) {
-
-    // Your code here
-
+  constructor(defaultSize = 4) {
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = [];
+    (() => {
+      for (let i = 0; i < defaultSize; i++) {
+        this.data[i] = undefined;
+      }
+    })();
   }
 
   read(index) {
-
-    // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
-
-    // Your code here
+    let newData = [];
+    if(val) {
+      this.length++;
+      newData[0] = val;
+      for (let i = 0; i < this.data.length; i++) {
+        newData[i + 1] = this.data[i]
+      }
+    }
+    this.data = newData;
   }
 
 }
